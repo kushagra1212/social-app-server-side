4978d507796307e8b5fd057f529711dda9018dac
kushagra1212 <kushagrarathore002@gmail.com>
2021-09-02 08:53:15 +0530

    Checking 2

--- routers/authfun.js
+++ routers/authfun.js
@@ -44,7 +44,7 @@ module.exports.sign_in = async (req, res) => {
 
     if (isYes) {
       const token = createToken(user._id);
-      const date = new Date();
+
       return res.cookie("jwt", token, { maxAge: maxAge * 1000, httpOnly: true,secure: process.env.NODE_ENV === "production"}).json({ message: "success", success: true, user: user });
     } else {
       res.send({ message: "Password Incorrect", success: false });
