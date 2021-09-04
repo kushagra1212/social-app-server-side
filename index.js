8325898147068a82ced2c8dedc7231c0f144efe5
kushagra1212 <kushagrarathore002@gmail.com>
2021-09-05 00:50:33 +0530

    x

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const express = require("express");
 
 const app = express();
 const cors = require("cors");
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
+app.use(cors({
+  credentials: true, origin: 'https://eimentum.vercel.app'}));
 
 const mongoose = require("mongoose");
 
