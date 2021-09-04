5fad22def58654a1ff3be6bfc0a1ccbd7005f65c
kushagra1212 <kushagrarathore002@gmail.com>
2021-09-05 02:36:05 +0530

    c

--- index.js
+++ index.js
@@ -8,15 +8,7 @@ const app = express();
 
 
 const mongoose = require("mongoose");
-app.use(function (req, res, next) {
-
-  res.setHeader('Access-Control-Allow-Origin', 'https://eimentum.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  next();
-});
+app.use(cors());
 
 const cookieParser = require("cookie-parser");
 const router = require("./routers/auth");
