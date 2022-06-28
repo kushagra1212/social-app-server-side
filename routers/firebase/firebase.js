9682af27abad9e42a9d8711e7f80bbf4fc592200
Kushagra Rathore <71135665+kushagra1212@users.noreply.github.com>
2022-06-28 18:58:12 +0530

    fixed

--- routers/firebase/firebase.js
+++ routers/firebase/firebase.js
@@ -1,7 +1,24 @@
+require('dotenv').config();
 const admin = require('firebase-admin');
-const serviceAccount=require("../../eimentum-firebase-adminsdk-34q6z-7537c5bbe8.json");
+
+
+
+
+
+
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      "type": procees.evn.type,
+      "project_id":  procees.evn.project_id,
+      "private_key_id":  procees.evn.private_key_id,
+      "private_key":  procees.evn.private_key,
+      "client_email": procees.evn.client_email,
+      "client_id":  procees.evn.client_id,
+      "auth_uri":  procees.evn.auth_uri,
+      "token_uri": procees.evn.token_uri,
+      "auth_provider_x509_cert_url": procees.evn.auth_provider_x509_cert_url,
+      "client_x509_cert_url":  procees.evn.client_x509_cert_url,
+    }),
     storageBucket:"eimentum.appspot.com"
   });
 
